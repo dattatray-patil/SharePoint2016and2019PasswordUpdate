6a6886d4b34d35f2a8724dd3c7c586fd9c982ae4
Dattatray Patil <60019982+dattatray-patil@users.noreply.github.com>
2020-05-23 17:53:01 +0100

    SharePoint 2016 and 2019 Farm&Service accounts Pwd Update

---
+++ PasswordResetUtility/PasswordResetUtility.ps1
@@ -0,0 +1,21 @@
+﻿#Below is for farm password change 
+#read xml file, skip root read like $config.destinationn
+#Translated XML config
+
+$config = ([xml](Get-Content LCTPasswordResetUtilityConfig.xml)).root
+
+
+$farmandserviceaccounts = $config.farmaccount.split(",");
+
+foreach($networkaccount in $farmandserviceaccounts)
+{ 
+
+   $PasswordVariableSecure = Read-Host "What is new password for farm/service account"  $networkaccount  " ? This field will be encrypted:" -AsSecureString
+
+   Set-SPManagedAccount -Identity  $networkaccount -ExistingPassword $PasswordVariableSecure -Confirm:$false
+
+}
+
+Write-Host "Password Changed Successfully! "
+
+iisreset
